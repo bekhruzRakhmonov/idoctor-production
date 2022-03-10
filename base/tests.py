5c88045c56e917f7410386eac4bec87a5f975500
bekhruzRakhmonov <bekhruzrakhmonov1@gmail.com>
2022-03-10 14:14:20 +0000

    make it better

--- base/tests.py
+++ base/tests.py
@@ -3,6 +3,6 @@ from django.test import TestCase
 class LoginTests(TestCase):
     def test_login(self):
         response = self.client.post(
-            '/login/', {"email": "bekhruzrakhmonov2@gmail.com", "password": "Idonotknow1@"})
+            '/login/', {"email": "bekhruzrakhmonov2@gmail.com", "password": "feruzbek2007"})
         print("test response",response)
         self.assertEqual(response.status_code,200)
