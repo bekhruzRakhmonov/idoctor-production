7eff15b786c8d92be9536e4b0346ad22fcfdcd23
bekhruzRakhmonov <85542200+bekhruzRakhmonov@users.noreply.github.com>
2022-06-03 07:29:16 +0500

    Update context_processors.py

--- base/utils/context_processors.py
+++ base/utils/context_processors.py
@@ -1,9 +1,5 @@
 from ..models import Like,Notification
 import datetime
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 def custom_context_processors(request):
 	likes = Like.objects.all()
@@ -14,7 +10,6 @@ def custom_context_processors(request):
 		return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
 	elif not request.user.is_anonymous and not request.user.is_authenticated:
 		notifications = Notification.objects.filter(to_anon_user__exact=request.user)
-		logger.info(f"hey guys how you are it is working {notifications}")
 		notf_count = Notification.get_count(to_anon_user=request.user)
 		return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
 	return {"likes":likes,"date_time":date_time}
