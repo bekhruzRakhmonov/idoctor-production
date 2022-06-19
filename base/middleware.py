c77ba858303aa204c56df81a4a4e998fba171fbe
bekhruzRakhmonov <85542200+bekhruzRakhmonov@users.noreply.github.com>
2022-06-19 16:43:15 +0500

    Update middleware.py

--- base/middleware.py
+++ base/middleware.py
@@ -7,6 +7,21 @@ from .cookies import b64_decode
 import json
 import time
 
+class RestrictUserAdminMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path.startswith(str(reverse_lazy('admin:index'))):
+            if request.user.is_authenticated:
+                if not request.user.is_staff:
+                    raise Http404
+            else:
+                raise Http404
+
+        response = self.get_response(request)
+        return response
+
 def get_user(request):
     if not hasattr(request, "_cached_user"):
         request._cached_user = auth.get_user(request)
