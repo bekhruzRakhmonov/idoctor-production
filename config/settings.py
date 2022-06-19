987c4e1c76a5c986885598cc66b5d100896a9cf0
bekhruzRakhmonov <85542200+bekhruzRakhmonov@users.noreply.github.com>
2022-06-19 16:44:34 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-
+    
+    "base.middleware.RestrictUserAdminMiddleware",
     "base.middleware.AnonUserMiddleware",
     "base.middleware.StatsMiddleware",
 ]
