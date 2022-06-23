70689320b239bfd52b4bd52014b2e3371ae5344e
Bexruz Raxmonov <bekhruzrakhmonov1@gmail.com>
2022-06-23 11:49:48 +0000

    deploy to aws

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["54.146.133.189","idoctor.eba-3ftk9ahp.us-east-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
