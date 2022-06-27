700e9527436e132553ef48568f3a5da8db57b012
Bexruz Raxmonov <bekhruzrakhmonov1@gmail.com>
2022-06-27 09:56:10 +0000

    deploy to aws

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["172.31.46.34","*"]
 
 # Application definition
 
