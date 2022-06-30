1577dbc603cac6a5ca0217af694b692348ca8668
Bexruz Raxmonov <bekhruzrakhmonov1@gmail.com>
2022-06-30 17:30:59 +0000

    static files issues fixed

--- config/urls.py
+++ config/urls.py
@@ -34,5 +34,5 @@ if settings.DEBUG:
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+#else:
+#    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
