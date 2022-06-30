d1f991cb146365a1ab6b9e471f01f6c7e8c3261c
Bexruz Raxmonov <bekhruzrakhmonov1@gmail.com>
2022-06-30 14:38:44 +0000

    aws s3 storage added

--- config/settings.py
+++ config/settings.py
@@ -198,8 +198,8 @@ COMPRESS_ENABLED = True
 
 # media
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "media/"
+#MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#MEDIA_URL = "media/"
 
 # AWS S3 Storage
 
