d064345809afb493b60564febbe944a6eaa7f4a7
Bexruz Raxmonov <bekhruzrakhmonov1@gmail.com>
2022-07-03 17:52:44 +0000

    make it better

--- base/utils/context_processors.py
+++ base/utils/context_processors.py
@@ -2,15 +2,15 @@ from ..models import Like,Notification
 import datetime
 
 def custom_context_processors(request):
-	likes = Like.objects.all()
-	date_time = str(datetime.datetime.now())[:10]
+    likes = Like.objects.all()
+    date_time = str(datetime.datetime.now())[:10]
     #media_url = "https://idoctorbucket.s3.amazonaws.com/"
-	if not request.user.is_anonymous and not request.user.is_anon:
-		notifications = Notification.objects.filter(to_user__exact=request.user)
-		notf_count = Notification.get_count(to_user=request.user)
-		return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
-	elif not request.user.is_anonymous and not request.user.is_authenticated:
-		notifications = Notification.objects.filter(to_anon_user__exact=request.user)
-		notf_count = Notification.get_count(to_anon_user=request.user)
-		return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
+    if not request.user.is_anonymous and not request.user.is_anon:
+        notifications = Notification.objects.filter(to_user__exact=request.user)
+        notf_count = Notification.get_count(to_user=request.user)
+        return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
+    elif not request.user.is_anonymous and not request.user.is_authenticated:
+        notifications = Notification.objects.filter(to_anon_user__exact=request.user)
+        notf_count = Notification.get_count(to_anon_user=request.user)
+        return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
     return {"likes":likes,"date_time":date_time}
