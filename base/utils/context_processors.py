6150ee0b73297d706309cc7778e7b6df32bc64cd
Bexruz Raxmonov <bekhruzrakhmonov1@gmail.com>
2022-07-03 17:45:59 +0000

    make it better

--- base/utils/context_processors.py
+++ base/utils/context_processors.py
@@ -4,6 +4,7 @@ import datetime
 def custom_context_processors(request):
 	likes = Like.objects.all()
 	date_time = str(datetime.datetime.now())[:10]
+    media_url = "https://idoctorbucket.s3.amazonaws.com/"
 	if not request.user.is_anonymous and not request.user.is_anon:
 		notifications = Notification.objects.filter(to_user__exact=request.user)
 		notf_count = Notification.get_count(to_user=request.user)
@@ -12,4 +13,4 @@ def custom_context_processors(request):
 		notifications = Notification.objects.filter(to_anon_user__exact=request.user)
 		notf_count = Notification.get_count(to_anon_user=request.user)
 		return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
-	return {"likes":likes,"date_time":date_time}
+    return {"likes":likes,"date_time":date_time,"media_url":media_url}
