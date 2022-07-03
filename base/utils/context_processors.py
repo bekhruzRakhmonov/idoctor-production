6ea866a3566e8ca6a7878e1bf8724eceda870108
Bexruz Raxmonov <bekhruzrakhmonov1@gmail.com>
2022-07-03 17:58:09 +0000

    make it better

--- base/utils/context_processors.py
+++ base/utils/context_processors.py
@@ -8,9 +8,9 @@ def custom_context_processors(request):
     if not request.user.is_anonymous and not request.user.is_anon:
         notifications = Notification.objects.filter(to_user__exact=request.user)
         notf_count = Notification.get_count(to_user=request.user)
-        return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
+        return {"likes":likes,"notf_count":notf_count,"notifications":notifications,"media_url":media_url}
     elif not request.user.is_anonymous and not request.user.is_authenticated:
         notifications = Notification.objects.filter(to_anon_user__exact=request.user)
         notf_count = Notification.get_count(to_anon_user=request.user)
-        return {"likes":likes,"notf_count":notf_count,"notifications":notifications}
+        return {"likes":likes,"notf_count":notf_count,"notifications":notifications,"media_url":media_url}
     return {"likes":likes,"date_time":date_time,"media_url":media_url}
