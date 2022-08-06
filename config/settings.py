5b0e1946f8fecd132ef51d1215e5cedb3068dc1b
bekhruzRakhmonov <85542200+bekhruzRakhmonov@users.noreply.github.com>
2022-08-06 18:17:54 +0500

    make it better

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,7 @@ import os
 from datetime import timedelta
 import asgiref
 import channels
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,14 +139,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'), # 'db'
-        'PORT': 5432,
-    }
+    "default": dj_database_url.config(conn_max_age=600),
 }
 
 # Password validation
